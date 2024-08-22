eaf1b3b13cc68e377a60adc50ffecebdfa0a7331
Marian Kannwischer <marian.kannwischer@t-online.de>
2024-08-22 16:48:23 +0200

    update default endpoint

--- rapidata/rapidata_client/rapidata_client.py
+++ rapidata/rapidata_client/rapidata_client.py
@@ -13,7 +13,7 @@ class RapidataClient:
         self,
         client_id: str,
         client_secret: str,
-        endpoint: str = "https://api.rapidata.ai",
+        endpoint: str = "https://api.app.rapidata.ai",
     ):
         """
         Initialize the RapidataClient.
