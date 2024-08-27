ad2e9b1ab9a0651d4438d4de703f51f64ac91aa3
Marian Kannwischer <marian.kannwischer@t-online.de>
2024-08-27 17:30:07 +0200

    add generated client

---
+++ openapi/openapi-client/openapi_client/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.coco_api import CocoApi
+from openapi_client.api.datapoint_api import DatapointApi
+from openapi_client.api.dataset_api import DatasetApi
+from openapi_client.api.identity_api import IdentityApi
+from openapi_client.api.newsletter_api import NewsletterApi
+from openapi_client.api.order_api import OrderApi
+from openapi_client.api.validation_api import ValidationApi
+
