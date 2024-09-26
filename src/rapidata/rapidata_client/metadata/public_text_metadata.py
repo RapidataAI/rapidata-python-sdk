3a2bc78800191a9bc8afdbcbad476a53e43a0e75
mads <mads-alber@hotmail.com>
2024-09-26 18:00:47 +0200

    add publicTextMetadata

---
+++ src/rapidata/rapidata_client/metadata/public_text_metadata.py
@@ -0,0 +1,16 @@
+from rapidata.api_client.models.public_text_metadata_input import (
+    PublicTextMetadataInput,
+)
+from rapidata.rapidata_client.metadata.base_metadata import Metadata
+
+
+class PublicTextMetadata(Metadata):
+
+    def __init__(self, text: str, identifier: str = "public_text"):
+        super().__init__(identifier=identifier)
+        self._text = text
+
+    def to_model(self):
+        return PublicTextMetadataInput(
+            _t="PublicTextMetadataInput", identifier=self._identifier, text=self._text
+        )
