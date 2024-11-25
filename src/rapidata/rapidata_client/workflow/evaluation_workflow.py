8e3a22206bf6ce70817fb0ad45a42eff28aa4e03
dsana <dania.sana.125@gmail.com>
2024-11-25 11:04:56 +0100

    The new workflow

--- src/rapidata/rapidata_client/workflow/evaluation_workflow.py
+++ src/rapidata/rapidata_client/workflow/evaluation_workflow.py
@@ -5,11 +5,12 @@ from rapidata.rapidata_client.workflow.base_workflow import Workflow
 class EvaluationWorkflow(Workflow):
     """Workflow to run evaluation orders. This is used internally only and should not be necessary to be used by clients."""
 
-    def __init__(self, validation_set_id: str):
+    def __init__(self, validation_set_id: str, should_accept_incorrect: bool):
         super().__init__("EvaluationWorkflow")
         self.validation_set_id = validation_set_id
+        self.should_accept_incorrect = should_accept_incorrect
 
     def to_model(self):
         return EvaluationWorkflowModel(
-            _t="EvaluationWorkflow", validationSetId=self.validation_set_id
+            _t="EvaluationWorkflow", validationSetId=self.validation_set_id, shouldAcceptIncorrect=self.should_accept_incorrect
         )
