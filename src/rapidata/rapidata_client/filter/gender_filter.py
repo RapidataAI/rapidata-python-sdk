036c2ed83877f7fec7f87a2e51af35c10221fbba
LinoGiger <linogiger@gmx.ch>
2024-12-10 11:08:35 +0100

    added doc strings to filters

--- src/rapidata/rapidata_client/filter/gender_filter.py
+++ src/rapidata/rapidata_client/filter/gender_filter.py
@@ -5,8 +5,17 @@ from rapidata.api_client.models.gender import Gender
 
 
 class GenderFilter(RapidataFilter):
+    """GenderFilter Class
+    
+    Can be used to filter who to target based on their gender."""
 
     def __init__(self, genders: list[Gender]):
+        """
+        Initialize a GenderFilter instance.
+
+        Args:
+            genders (list[Gender]): List of genders to filter by.
+        """
         self.genders = genders
 
     def to_model(self):
