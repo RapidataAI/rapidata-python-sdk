23d8cd04b5692f0c31b0ed7bbc2775b0ace4c11c
LinoGiger <linogiger@gmx.ch>
2024-12-11 11:34:15 +0100

    added doc strings for selections

--- src/rapidata/rapidata_client/selection/labeling_selection.py
+++ src/rapidata/rapidata_client/selection/labeling_selection.py
@@ -6,8 +6,17 @@ from rapidata.api_client.models.labeling_selection import (
 
 
 class LabelingSelection(RapidataSelection):
+    """Labeling selection class.
+    
+    Decides how many actual datapoints you want to show per session."""
 
     def __init__(self, amount: int):
+        """
+        Initialize a LabelingSelection instance.
+
+        Args:
+            amount (int): The amount of labeling rapids that will be shown per session.
+        """
         self.amount = amount
 
     def to_model(self) -> Any:
