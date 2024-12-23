dd9d19c7783f29638fa23d0d6318373fc91630ed
sneccello <marcellschneider@icloud.com>
2024-12-23 10:06:11 +0100

    finishing heatmaps

--- src/rapidata/annot/create_rich_annot_heatmaps.py
+++ src/rapidata/annot/create_rich_annot_heatmaps.py
@@ -1,6 +1,7 @@
 import os
 from io import BytesIO
 
+import dotenv
 import numpy as np
 import requests
 from PIL import Image
@@ -9,15 +10,16 @@ from rapidata.annot.api import create_client
 import seaborn as sns
 from scipy.ndimage import gaussian_filter
 
-from rapidata.annot.consts import PRODUCTION_ENVIRONMENT
+from rapidata.annot.consts import PRODUCTION_ENVIRONMENT, TEST_ENVIRONMENT, DOTENV_PATH
 
-LOCATE_ORDER_ID = "6759b2e21c406a176a8283b0"
-RICH_ANNOT_FOLDER = "/Users/marcellschneider/Documents/rapidata/rapidata-python-sdk/src/rapidata/annot/rich_annotations/Example data"
+LOCATE_ORDER_ID = "6769269efd1e06f7615767fc"
+IMAGE_FOLDER = "/Users/marcellschneider/Documents/rapidata/rapidata-python-sdk/src/rapidata/annot/rich_annotations/Example data"
 # heatmaps uses a gaussian curve with a sigma. This parameter controls,
 # what should be the sigma compared to min(image.width, image.height)
 # e.g. TOUCH_BELL_CURVE_SIGMA_TO_IMAGE_RATIO=0.5 if the image is (300, 500) the sigma will be 150
 # basically comes down how fat the finger is
 TOUCH_BELL_CURVE_SIGMA_TO_IMAGE_RATIO = 0.1
+ENVIRONMENT = TEST_ENVIRONMENT
 
 def create_heatmap(image_path, points, output_path):
     image = Image.open(image_path)
@@ -42,16 +44,17 @@ def create_heatmap(image_path, points, output_path):
 
 
 def main():
+    dotenv.load_dotenv(DOTENV_PATH)
     OUTPUT_FOLDER = f"heatmaps_{LOCATE_ORDER_ID}"
 
     os.makedirs(OUTPUT_FOLDER, exist_ok=True)
-    client = create_client(PRODUCTION_ENVIRONMENT)
+    client = create_client(ENVIRONMENT)
     order = client.order.get_order_by_id(LOCATE_ORDER_ID)
     results = order.get_results()
 
     for rapid in results["results"]:
         image_name = rapid["originalFileName"]
-        image_path = os.path.join(RICH_ANNOT_FOLDER, image_name)
+        image_path = os.path.join(IMAGE_FOLDER, image_name)
 
         points = [(int(p['x']), int(p['y'])) for p in rapid["aggregatedResults"]]
         create_heatmap(image_path, points, os.path.join(OUTPUT_FOLDER, image_name))
