d34b571f1fd94219326171abcbc74a540b88b290
LinoGiger <linogiger@gmx.ch>
2025-01-28 09:55:14 +0100

    added better sessions description

--- src/rapidata/rapidata_client/selection/rapidata_selections.py
+++ src/rapidata/rapidata_client/selection/rapidata_selections.py
@@ -8,7 +8,16 @@ from rapidata.rapidata_client.selection import (
 class RapidataSelections:
     """RapidataSelections Classes
 
-    Selections are used to define what type of tasks and in what order they are shown to the user.
+    Selections are used to define what type of tasks and in what order they are shown to the user. 
+    All Tasks are called a "Session". A session can contain multiple tasks of different types.
+
+    Example:
+    ```python
+        selections=[ValidationSelection("your-validation-set-id", 1), 
+                    LabelingSelection(2)]
+    ```
+
+    The above example will create a session with a validation task followed by a labeling task.
 
     Attributes:
         labeling (LabelingSelection): The LabelingSelection instance.
@@ -16,6 +25,7 @@ class RapidataSelections:
         conditional_validation (ConditionalValidationSelection): The ConditionalValidationSelection instance.
         demographic (DemographicSelection): The DemographicSelection instance.
         capped (CappedSelection): The CappedSelection instance."""
+    
     labeling = LabelingSelection
     validation = ValidationSelection
     conditional_validation = ConditionalValidationSelection
