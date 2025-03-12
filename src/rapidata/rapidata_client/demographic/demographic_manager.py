26b780b97b33f08f0101a82d5f4d9665cedc85a8
LinoGiger <linogiger@gmx.ch>
2025-03-12 17:59:55 +0100

    fixed creation of demographic with link

--- src/rapidata/rapidata_client/demographic/demographic_manager.py
+++ src/rapidata/rapidata_client/demographic/demographic_manager.py
@@ -2,6 +2,8 @@ from rapidata.service.openapi_service import OpenAPIService
 from rapidata.rapidata_client.assets import MediaAsset
 from rapidata.api_client.models.create_demographic_rapid_model import CreateDemographicRapidModel
 from rapidata.api_client.models.classify_payload import ClassifyPayload
+import requests
+from requests.adapters import HTTPAdapter, Retry
 
 
 class DemographicManager:
@@ -23,5 +25,38 @@ class DemographicManager:
                 title=instruction
             )
         )
+        session = self._get_session()
+        media.session = session
+        
         self._openapi_service.rapid_api.rapid_create_demographic_rapid_post(model=model, file=[media.to_file()])
+
+    def _get_session(self, max_retries: int = 5, max_workers: int = 10) -> requests.Session:   
+        """Get a requests session with retry logic.
+
+
+        Args:
+            max_retries (int): The maximum number of retries.
+            max_workers (int): The maximum number of workers.
+
+        Returns:
+            requests.Session: A requests session with retry logic.
+        """
+        session = requests.Session()
+        retries = Retry(
+            total=max_retries,
+            backoff_factor=1,
+            status_forcelist=[500, 502, 503, 504],
+            allowed_methods=["GET"],
+            respect_retry_after_header=True
+        )
+
+        adapter = HTTPAdapter(
+            pool_connections=max_workers * 2,
+            pool_maxsize=max_workers * 4,
+            max_retries=retries
+        )
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+
+        return session
         
