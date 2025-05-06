1566152143a384f83bd5b7c125d1ccff59b07208
LinoGiger <linogiger@gmx.ch>
2025-05-06 15:35:59 +0200

    Added retrieval modes for labeling selection

---
+++ src/rapidata/rapidata_client/selection/retrieval_modes.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class RetrievalMode(Enum):
+    Random = "Random"
+    """Will just randomly shuffle the datapoints. This is the default and will NOT take into account the "max_iterations" parameter."""
+    Shuffled = "Shuffled"
+    """Will shuffle the datapoints randomly for each user. The user will then see the datapoints in that order. This will take into account the "max_iterations" parameter."""
+    Sequential = "Sequential"
+    """Will show the datapoints in the order they are in the dataset. This will take into account the "max_iterations" parameter."""
