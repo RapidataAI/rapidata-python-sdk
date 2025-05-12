c86a0c0396bc051b5c862589f3cfef618d185589
LinoGiger <linogiger@gmx.ch>
2025-05-12 13:57:18 +0200

    moved comments to doc strings for documents

--- src/rapidata/rapidata_client/selection/retrieval_modes.py
+++ src/rapidata/rapidata_client/selection/retrieval_modes.py
@@ -1,9 +1,18 @@
 from enum import Enum
 
 class RetrievalMode(Enum):
-    # Will just randomly shuffle the datapoints. This is the default and will NOT take into account the "max_iterations" parameter.
+    """
+    Enum for defining retrieval modes for datapoints.
+    """
     Random = "Random"
-    # Will shuffle the datapoints randomly for each user. The user will then see the datapoints in that order. This will take into account the "max_iterations" parameter.
+    """
+    Will just randomly shuffle the datapoints. This is the default and will NOT take into account the "max_iterations" parameter.
+    """
     Shuffled = "Shuffled"
-    # Will show the datapoints in the order they are in the dataset. This will take into account the "max_iterations" parameter.
+    """
+    Will shuffle the datapoints randomly for each user. The user will then see the datapoints in that order. This will take into account the "max_iterations" parameter.
+    """
     Sequential = "Sequential"
+    """
+    Will show the datapoints in the order they are in the dataset. This will take into account the "max_iterations" parameter.
+    """
