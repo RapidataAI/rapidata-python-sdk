f33d241d2be53f40b27fcc971732a15ebf15fd2b
LinoGiger <linogiger@gmx.ch>
2025-05-27 11:53:52 +0200

    added one more example image

--- examples/basic_locate_order.py
+++ examples/basic_locate_order.py
@@ -2,7 +2,8 @@ from rapidata import RapidataClient, RapidataSettings
 
 IMAGE_URLS = ["https://assets.rapidata.ai/eac11c3e-ad57-402b-90ed-23378d2ff869.jpg",
               "https://assets.rapidata.ai/04e7e3c6-5554-47ca-bdb2-950e48ac3e6c.jpg",
-              "https://assets.rapidata.ai/91d9913c-b399-47f8-ad19-767798cc951c.jpg"]
+              "https://assets.rapidata.ai/91d9913c-b399-47f8-ad19-767798cc951c.jpg",
+              "https://assets.rapidata.ai/d1cbf51d-7712-4819-96b4-20e030c573de.jpg"]
 
     
 if __name__ == "__main__":
