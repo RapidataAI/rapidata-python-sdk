304cea2e64947bafb47aa741e2a05cbf20fde5a3
Yohoji <etha3leb@gmail.com>
2025-06-20 09:23:22 +0100

    feat: add login button

---
+++ docs/js/login.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const nav = document.querySelector('.md-header__inner')
+  console.log('nav', nav)
+  if (!nav) return;
+  const btn = document.createElement('a');
+  btn.href = 'https://app.rapidata.ai';
+  btn.target = '_blank'
+  btn.className = 'login-button';
+  btn.textContent = 'Login';
+  btn.title = 'Login to rapidata'
+  nav.appendChild(btn);
+});
