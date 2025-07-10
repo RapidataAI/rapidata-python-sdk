838e7e4504350bb658770960c68a79ab199c94d5
Lino Giger <68745352+LinoGiger@users.noreply.github.com>
2025-07-10 11:34:52 +0200

    added prompt identifier as class

---
+++ src/rapidata/rapidata_client/metadata/_prompt_identifier_metadata.py
@@ -0,0 +1,15 @@
+from rapidata.rapidata_client.metadata._base_metadata import Metadata
+from rapidata.api_client.models.private_text_metadata_input import (
+    PrivateTextMetadataInput,
+)
+
+
+class PromptIdentifierMetadata(Metadata):
+    def __init__(self, identifier: str):
+        super().__init__()
+        self._identifier = identifier
+
+    def to_model(self):
+        return PrivateTextMetadataInput(
+            _t="PrivateTextMetadataInput", identifier="prompt-id", text=self._identifier
+        )
