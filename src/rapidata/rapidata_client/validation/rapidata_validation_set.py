f87e3f8abd04b68f2c67348cff84323df479a30a
LinoGiger <68745352+LinoGiger@users.noreply.github.com>
2025-07-22 18:15:47 +0200

    option to disable alerts for validationset (#319)
    * option to disable alerts for validationset
    
    * adjusted doc string

--- src/rapidata/rapidata_client/validation/rapidata_validation_set.py
+++ src/rapidata/rapidata_client/validation/rapidata_validation_set.py
@@ -2,7 +2,7 @@ from rapidata.rapidata_client.validation.rapids.rapids import Rapid
 from rapidata.service.openapi_service import OpenAPIService
 from rapidata.rapidata_client.logging import logger
 from rapidata.api_client.models.update_dimensions_model import UpdateDimensionsModel
-from rapidata.rapidata_client.datapoints.assets._sessions import SessionManager
+from rapidata.api_client.models.update_should_alert_model import UpdateShouldAlertModel
 
 class RapidataValidationSet:
     """A class for interacting with a Rapidata validation set.
@@ -38,6 +38,22 @@ class RapidataValidationSet:
         """
         logger.debug(f"Updating dimensions for validation set {self.id} to {dimensions}")
         self.__openapi_service.validation_api.validation_set_validation_set_id_dimensions_put(self.id, UpdateDimensionsModel(dimensions=dimensions))
+        return self
+    
+    def update_should_alert(self, should_alert: bool):
+        """Determines whether users should be alerted if they answer incorrectly.
+
+        Args:
+            should_alert (bool): Specifies whether users should be alerted for incorrect answers. Defaults to True if not specifically overridden by this method.
+
+        Note:
+            The userScore dimensions which are updated when a user answers a validation task are updated regardless of the value of `should_alert`.
+        """
+        logger.debug(f"Setting shouldAlert for validation set {self.id} to {should_alert}")
+        self.__openapi_service.validation_api.validation_set_validation_set_id_shouldalert_patch(
+            self.id, 
+            UpdateShouldAlertModel(shouldAlert=should_alert)
+        )
         return self
 
     def __str__(self):
