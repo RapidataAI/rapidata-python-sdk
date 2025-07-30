e4ff63a7c44e22dbb194a21e48f300ae186cf576
Lino Giger <68745352+LinoGiger@users.noreply.github.com>
2025-07-30 14:18:46 +0200

    added ability to adjust level of detail on leaderboard

---
+++ src/rapidata/rapidata_client/benchmark/_detail_mapper.py
@@ -0,0 +1,32 @@
+from typing import Literal
+
+
+class DetailMapper:
+    MIN_RESPONSES = 3
+
+    @staticmethod
+    def get_budget(level_of_detail: Literal["low", "medium", "high", "extreme"]) -> int:
+        if level_of_detail == "low":
+            return 2_000
+        elif level_of_detail == "medium":
+            return 4_000
+        elif level_of_detail == "high":
+            return 8_000
+        elif level_of_detail == "extreme":
+            return 16_000
+        else:
+            raise ValueError(
+                "Invalid level of detail. Must be one of: 'low', 'medium', 'high', 'extreme'"
+            )
+
+    @staticmethod
+    def get_level_of_detail(
+        response_budget: int,
+    ) -> Literal["low", "medium", "high", "extreme"]:
+        if response_budget <= 3_999:
+            return "low"
+        elif response_budget <= 7_999:
+            return "medium"
+        elif response_budget <= 15_999:
+            return "high"
+        return "extreme"
