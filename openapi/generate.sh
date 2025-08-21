782d60054850436c6b82ec2bf6e80b3e980487b6
LinoGiger <68745352+LinoGiger@users.noreply.github.com>
2025-08-21 10:31:22 +0200

    Update generate.sh

--- openapi/generate.sh
+++ openapi/generate.sh
@@ -1,1 +1,6 @@
-npx openapi-generator-cli generate --global-property modelTests=false,modelDocs=false,apiTests=false,apiDocs=false -i openapi/output.swagger.json -g python -o ./src -c openapi/config.yaml -t openapi/templates
+#!/bin/bash
+# Filter out deprecated endpoints
+jq 'del(.paths[][] | select(.deprecated == true))' openapi/output.swagger.json > openapi/filtered.swagger.json
+
+# Generate client using filtered spec
+npx openapi-generator-cli generate --global-property modelTests=false,modelDocs=false,apiTests=false,apiDocs=false -i openapi/filtered.swagger.json -g python -o ./src -c openapi/config.yaml -t openapi/templates
