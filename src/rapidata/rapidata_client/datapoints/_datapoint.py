7fd9d4a401784d38861e81b8d2d9776e89748f06
Lino Giger <68745352+LinoGiger@users.noreply.github.com>
2025-09-25 10:35:40 +0200

    adjusted validation set recommendation with types

--- src/rapidata/rapidata_client/datapoints/_datapoint.py
+++ src/rapidata/rapidata_client/datapoints/_datapoint.py
@@ -2,6 +2,14 @@ from typing import Literal
 
 from pydantic import BaseModel, model_validator
 from typing_extensions import Self
+from rapidata.rapidata_client.datapoints.assets.constants import (
+    ALLOWED_VIDEO_EXTENSIONS,
+    ALLOWED_IMAGE_EXTENSIONS,
+    ALLOWED_AUDIO_EXTENSIONS,
+)
+from rapidata.api_client.models.asset_type import AssetType
+from rapidata.api_client.models.prompt_type import PromptType
+from rapidata.rapidata_client.config import logger
 
 
 class Datapoint(BaseModel):
@@ -11,6 +19,7 @@ class Datapoint(BaseModel):
     media_context: str | None = None
     sentence: str | None = None
     private_note: str | None = None
+    logger = logger
 
     @model_validator(mode="after")
     def check_sentence_and_context(self) -> Self:
@@ -19,3 +28,34 @@ class Datapoint(BaseModel):
                 "Both 'sentence' and 'context' cannot be strings at the same time."
             )
         return self
+
+    def get_asset_type(self) -> AssetType:
+        if self.data_type == "text":
+            return AssetType.TEXT
+
+        evaluation_asset = (
+            self.assets[0] if isinstance(self.assets, list) else self.assets
+        )
+        if any(evaluation_asset.endswith(ext) for ext in ALLOWED_IMAGE_EXTENSIONS):
+            return AssetType.IMAGE
+        elif any(evaluation_asset.endswith(ext) for ext in ALLOWED_VIDEO_EXTENSIONS):
+            return AssetType.VIDEO
+        elif any(evaluation_asset.endswith(ext) for ext in ALLOWED_AUDIO_EXTENSIONS):
+            return AssetType.AUDIO
+        else:
+            logger.debug(
+                f"Cannot get asset type for asset type: {type(evaluation_asset)}"
+            )
+            return AssetType.NONE
+
+    def get_prompt_type(self) -> list[PromptType]:
+        prompt_types = []
+        if self.context:
+            prompt_types.append(PromptType.TEXT)
+        if self.media_context:
+            prompt_types.append(PromptType.ASSET)
+
+        if len(prompt_types) == 0:
+            return [PromptType.NONE]
+
+        return prompt_types
