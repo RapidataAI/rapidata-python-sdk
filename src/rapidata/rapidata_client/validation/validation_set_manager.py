058aa63e4497f2eced63bcf621708da2d5a92bdf
Lino Giger <68745352+LinoGiger@users.noreply.github.com>
2025-10-03 10:44:34 +0200

    general upkeeping

--- src/rapidata/rapidata_client/validation/validation_set_manager.py
+++ src/rapidata/rapidata_client/validation/validation_set_manager.py
@@ -672,13 +672,13 @@ class ValidationSetManager:
             )
 
     def find_validation_sets(
-        self, name: str = "", amount: int = 1
+        self, name: str = "", amount: int = 10
     ) -> list[RapidataValidationSet]:
         """Find validation sets by name.
 
         Args:
             name (str, optional): The name to search for. Defaults to "" to match with any set.
-            amount (int, optional): The amount of validation sets to return. Defaults to 1.
+            amount (int, optional): The amount of validation sets to return. Defaults to 10.
 
         Returns:
             list[RapidataValidationSet]: The list of validation sets.
