b3aa918b4e98d0ac3220cc6edf551f5d5b214da8
LinoGiger <LinoGiger@users.noreply.github.com>
2025-11-06 09:27:39 +0000

    deploy: b289e9729a26d9fc0f117ebafa1e575f573f753e

---
+++ js/tawk.js
@@ -0,0 +1,11 @@
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function () {
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = "https://embed.tawk.to/671787452480f5b4f5914892/1iapugqdk";
+  s1.charset = "UTF-8";
+  s1.setAttribute("crossorigin", "*");
+  s0.parentNode.insertBefore(s1, s0);
+})();
