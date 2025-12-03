f0d5a7c1974005aa2edc360371ab50c1bf108410
Jorge Paravicini <24812622+jorgeparavicini@users.noreply.github.com>
2025-12-03 16:18:20 +0100

    feat: update schema generator

--- openapi/generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Filter out deprecated endpoints
-jq 'del(.paths[][] | select(.deprecated == true))' openapi/output.swagger.json > openapi/filtered.swagger.json
-
-# Generate client using filtered spec
-npx openapi-generator-cli generate --global-property modelTests=false,modelDocs=false,apiTests=false,apiDocs=false -i openapi/filtered.swagger.json -g python -o ./src -c openapi/config.yaml -t openapi/templates
