5dcfc084b41bf268b668899a6a983e5cb8821a76
Rudolf the Rabbit <login@rapidata.ai>
2026-01-19 10:49:17 +0000

    chore: update OpenAPI client to 2026.01.19.1039-3f048a9

--- src/rapidata/api_client/api/benchmark_api.py
+++ src/rapidata/api_client/api/benchmark_api.py
@@ -36,6 +36,7 @@ from rapidata.api_client.models.standings_by_benchmark_result import StandingsBy
 from rapidata.api_client.models.submit_prompt_model import SubmitPromptModel
 from rapidata.api_client.models.tags_by_benchmark_result import TagsByBenchmarkResult
 from rapidata.api_client.models.update_benchmark_model import UpdateBenchmarkModel
+from rapidata.api_client.models.vote_matrix_result import VoteMatrixResult
 
 from rapidata.api_client.api_client import ApiClient, RequestSerialized
 from rapidata.api_client.api_response import ApiResponse
@@ -1096,6 +1097,346 @@ class BenchmarkApi:
         return self.api_client.param_serialize(
             method='GET',
             resource_path='/benchmark/{benchmarkId}/leaderboards',
+            path_params=_path_params,
+            query_params=_query_params,
+            header_params=_header_params,
+            body=_body_params,
+            post_params=_form_params,
+            files=_files,
+            auth_settings=_auth_settings,
+            collection_formats=_collection_formats,
+            _host=_host,
+            _request_auth=_request_auth
+        )
+
+
+
+
+    @validate_call
+    def benchmark_benchmark_id_matrix_get(
+        self,
+        benchmark_id: StrictStr,
+        tags: Optional[List[StrictStr]] = None,
+        participant_ids: Optional[List[StrictStr]] = None,
+        leaderboard_ids: Optional[List[StrictStr]] = None,
+        use_weighted_scoring: Optional[StrictBool] = None,
+        _request_timeout: Union[
+            None,
+            Annotated[StrictFloat, Field(gt=0)],
+            Tuple[
+                Annotated[StrictFloat, Field(gt=0)],
+                Annotated[StrictFloat, Field(gt=0)]
+            ]
+        ] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> VoteMatrixResult:
+        """Returns the pairwise vote matrix for a benchmark.
+
+
+        :param benchmark_id: (required)
+        :type benchmark_id: str
+        :param tags:
+        :type tags: List[str]
+        :param participant_ids:
+        :type participant_ids: List[str]
+        :param leaderboard_ids:
+        :type leaderboard_ids: List[str]
+        :param use_weighted_scoring:
+        :type use_weighted_scoring: bool
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :type _request_timeout: int, tuple(int, int), optional
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the
+                              authentication in the spec for a single request.
+        :type _request_auth: dict, optional
+        :param _content_type: force content-type for the request.
+        :type _content_type: str, Optional
+        :param _headers: set to override the headers for a single
+                         request; this effectively ignores the headers
+                         in the spec for a single request.
+        :type _headers: dict, optional
+        :param _host_index: set to override the host_index for a single
+                            request; this effectively ignores the host_index
+                            in the spec for a single request.
+        :type _host_index: int, optional
+        :return: Returns the result object.
+        """ # noqa: E501
+
+        _param = self._benchmark_benchmark_id_matrix_get_serialize(
+            benchmark_id=benchmark_id,
+            tags=tags,
+            participant_ids=participant_ids,
+            leaderboard_ids=leaderboard_ids,
+            use_weighted_scoring=use_weighted_scoring,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index
+        )
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            '200': "VoteMatrixResult",
+            '400': "ValidationProblemDetails",
+            '401': None,
+            '403': None,
+        }
+        response_data = self.api_client.call_api(
+            *_param,
+            _request_timeout=_request_timeout
+        )
+        response_data.read()
+        return self.api_client.response_deserialize(
+            response_data=response_data,
+            response_types_map=_response_types_map,
+        ).data
+
+
+    @validate_call
+    def benchmark_benchmark_id_matrix_get_with_http_info(
+        self,
+        benchmark_id: StrictStr,
+        tags: Optional[List[StrictStr]] = None,
+        participant_ids: Optional[List[StrictStr]] = None,
+        leaderboard_ids: Optional[List[StrictStr]] = None,
+        use_weighted_scoring: Optional[StrictBool] = None,
+        _request_timeout: Union[
+            None,
+            Annotated[StrictFloat, Field(gt=0)],
+            Tuple[
+                Annotated[StrictFloat, Field(gt=0)],
+                Annotated[StrictFloat, Field(gt=0)]
+            ]
+        ] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> ApiResponse[VoteMatrixResult]:
+        """Returns the pairwise vote matrix for a benchmark.
+
+
+        :param benchmark_id: (required)
+        :type benchmark_id: str
+        :param tags:
+        :type tags: List[str]
+        :param participant_ids:
+        :type participant_ids: List[str]
+        :param leaderboard_ids:
+        :type leaderboard_ids: List[str]
+        :param use_weighted_scoring:
+        :type use_weighted_scoring: bool
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :type _request_timeout: int, tuple(int, int), optional
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the
+                              authentication in the spec for a single request.
+        :type _request_auth: dict, optional
+        :param _content_type: force content-type for the request.
+        :type _content_type: str, Optional
+        :param _headers: set to override the headers for a single
+                         request; this effectively ignores the headers
+                         in the spec for a single request.
+        :type _headers: dict, optional
+        :param _host_index: set to override the host_index for a single
+                            request; this effectively ignores the host_index
+                            in the spec for a single request.
+        :type _host_index: int, optional
+        :return: Returns the result object.
+        """ # noqa: E501
+
+        _param = self._benchmark_benchmark_id_matrix_get_serialize(
+            benchmark_id=benchmark_id,
+            tags=tags,
+            participant_ids=participant_ids,
+            leaderboard_ids=leaderboard_ids,
+            use_weighted_scoring=use_weighted_scoring,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index
+        )
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            '200': "VoteMatrixResult",
+            '400': "ValidationProblemDetails",
+            '401': None,
+            '403': None,
+        }
+        response_data = self.api_client.call_api(
+            *_param,
+            _request_timeout=_request_timeout
+        )
+        response_data.read()
+        return self.api_client.response_deserialize(
+            response_data=response_data,
+            response_types_map=_response_types_map,
+        )
+
+
+    @validate_call
+    def benchmark_benchmark_id_matrix_get_without_preload_content(
+        self,
+        benchmark_id: StrictStr,
+        tags: Optional[List[StrictStr]] = None,
+        participant_ids: Optional[List[StrictStr]] = None,
+        leaderboard_ids: Optional[List[StrictStr]] = None,
+        use_weighted_scoring: Optional[StrictBool] = None,
+        _request_timeout: Union[
+            None,
+            Annotated[StrictFloat, Field(gt=0)],
+            Tuple[
+                Annotated[StrictFloat, Field(gt=0)],
+                Annotated[StrictFloat, Field(gt=0)]
+            ]
+        ] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> RESTResponseType:
+        """Returns the pairwise vote matrix for a benchmark.
+
+
+        :param benchmark_id: (required)
+        :type benchmark_id: str
+        :param tags:
+        :type tags: List[str]
+        :param participant_ids:
+        :type participant_ids: List[str]
+        :param leaderboard_ids:
+        :type leaderboard_ids: List[str]
+        :param use_weighted_scoring:
+        :type use_weighted_scoring: bool
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :type _request_timeout: int, tuple(int, int), optional
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the
+                              authentication in the spec for a single request.
+        :type _request_auth: dict, optional
+        :param _content_type: force content-type for the request.
+        :type _content_type: str, Optional
+        :param _headers: set to override the headers for a single
+                         request; this effectively ignores the headers
+                         in the spec for a single request.
+        :type _headers: dict, optional
+        :param _host_index: set to override the host_index for a single
+                            request; this effectively ignores the host_index
+                            in the spec for a single request.
+        :type _host_index: int, optional
+        :return: Returns the result object.
+        """ # noqa: E501
+
+        _param = self._benchmark_benchmark_id_matrix_get_serialize(
+            benchmark_id=benchmark_id,
+            tags=tags,
+            participant_ids=participant_ids,
+            leaderboard_ids=leaderboard_ids,
+            use_weighted_scoring=use_weighted_scoring,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index
+        )
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            '200': "VoteMatrixResult",
+            '400': "ValidationProblemDetails",
+            '401': None,
+            '403': None,
+        }
+        response_data = self.api_client.call_api(
+            *_param,
+            _request_timeout=_request_timeout
+        )
+        return response_data.response
+
+
+    def _benchmark_benchmark_id_matrix_get_serialize(
+        self,
+        benchmark_id,
+        tags,
+        participant_ids,
+        leaderboard_ids,
+        use_weighted_scoring,
+        _request_auth,
+        _content_type,
+        _headers,
+        _host_index,
+    ) -> RequestSerialized:
+
+        _host = None
+
+        _collection_formats: Dict[str, str] = {
+            'tags': 'multi',
+            'participantIds': 'multi',
+            'leaderboardIds': 'multi',
+        }
+
+        _path_params: Dict[str, str] = {}
+        _query_params: List[Tuple[str, str]] = []
+        _header_params: Dict[str, Optional[str]] = _headers or {}
+        _form_params: List[Tuple[str, str]] = []
+        _files: Dict[
+            str, Union[str, bytes, List[str], List[bytes], List[Tuple[str, bytes]]]
+        ] = {}
+        _body_params: Optional[bytes] = None
+
+        # process the path parameters
+        if benchmark_id is not None:
+            _path_params['benchmarkId'] = benchmark_id
+        # process the query parameters
+        if tags is not None:
+            
+            _query_params.append(('tags', tags))
+            
+        if participant_ids is not None:
+            
+            _query_params.append(('participantIds', participant_ids))
+            
+        if leaderboard_ids is not None:
+            
+            _query_params.append(('leaderboardIds', leaderboard_ids))
+            
+        if use_weighted_scoring is not None:
+            
+            _query_params.append(('useWeightedScoring', use_weighted_scoring))
+            
+        # process the header parameters
+        # process the form parameters
+        # process the body parameter
+
+
+        # set the HTTP header `Accept`
+        if 'Accept' not in _header_params:
+            _header_params['Accept'] = self.api_client.select_header_accept(
+                [
+                    'application/json'
+                ]
+            )
+
+
+        # authentication setting
+        _auth_settings: List[str] = [
+            'OAuth2', 
+            'OpenIdConnect', 
+            'Bearer'
+        ]
+
+        return self.api_client.param_serialize(
+            method='GET',
+            resource_path='/benchmark/{benchmarkId}/matrix',
             path_params=_path_params,
             query_params=_query_params,
             header_params=_header_params,
