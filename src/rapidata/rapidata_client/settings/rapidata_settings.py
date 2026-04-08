653b8f2721eab55f9f1ce25fc680382961e4cbad
LinoGiger <68745352+LinoGiger@users.noreply.github.com>
2026-04-08 16:18:53 +0200

    moved play video until the end to play percentage video (#523)
    * moved play video until the end to play percentage video
    
    * fixed boundries

--- src/rapidata/rapidata_client/settings/rapidata_settings.py
+++ src/rapidata/rapidata_client/settings/rapidata_settings.py
@@ -2,7 +2,7 @@ from rapidata.rapidata_client.settings.alert_on_fast_response import AlertOnFast
 from rapidata.rapidata_client.settings.translation_behaviour import TranslationBehaviourSetting
 from rapidata.rapidata_client.settings.free_text_minimum_characters import FreeTextMinimumCharactersSetting
 from rapidata.rapidata_client.settings.no_shuffle import NoShuffleSetting
-from rapidata.rapidata_client.settings.play_video_until_the_end import PlayVideoUntilTheEndSetting
+from rapidata.rapidata_client.settings.play_percentage_video import PlayPercentageVideoSetting
 from rapidata.rapidata_client.settings.allow_neither_both import AllowNeitherBothSetting
 from rapidata.rapidata_client.settings.swap_context_instruction import SwapContextInstructionSetting
 from rapidata.rapidata_client.settings.mute_video import MuteVideoSetting
@@ -20,7 +20,7 @@ class RapidataSettings:
         TranslationBehaviour (TranslationBehaviourSetting): Defines what's the behaviour of the translation in the UI.
         FreeTextMinimumCharacters (FreeTextMinimumCharactersSetting): Only for free text tasks. Set the minimum number of characters a user has to type.
         NoShuffle (NoShuffleSetting): Only for classification and compare tasks. If true, the order of the categories / images will not be shuffled and presented in the same order as specified.
-        PlayVideoUntilTheEnd (PlayVideoUntilTheEndSetting): Allows users to only answer once the video has finished playing.
+        PlayPercentageVideo (PlayPercentageVideoSetting): Requires users to watch a certain percentage of the video before they can submit their answer.
         AllowNeitherBoth (AllowNeitherBothSetting): Only for compare tasks. If true, the users will be able to select neither or both instead of exclusively one of the options.
         SwapContextInstruction (SwapContextInstructionSetting): Swap the place of the context and instruction.
         MuteVideo (MuteVideoSetting): Mute the video.
@@ -39,14 +39,14 @@ class RapidataSettings:
     TranslationBehaviour = TranslationBehaviourSetting
     FreeTextMinimumCharacters = FreeTextMinimumCharactersSetting
     NoShuffle = NoShuffleSetting
-    PlayVideoUntilTheEnd = PlayVideoUntilTheEndSetting
+    PlayPercentageVideo = PlayPercentageVideoSetting
     AllowNeitherBoth = AllowNeitherBothSetting
     SwapContextInstruction = SwapContextInstructionSetting
     MuteVideo = MuteVideoSetting
     Markdown = MarkdownSetting
 
     def __str__(self) -> str:
-        return f"RapidataSettings(AlertOnFastResponse={self.AlertOnFastResponse}, TranslationBehaviour={self.TranslationBehaviour}, FreeTextMinimumCharacters={self.FreeTextMinimumCharacters}, NoShuffle={self.NoShuffle}, PlayVideoUntilTheEnd={self.PlayVideoUntilTheEnd}, AllowNeitherBoth={self.AllowNeitherBoth}, SwapContextInstruction={self.SwapContextInstruction}, MuteVideo={self.MuteVideo})"
+        return f"RapidataSettings(AlertOnFastResponse={self.AlertOnFastResponse}, TranslationBehaviour={self.TranslationBehaviour}, FreeTextMinimumCharacters={self.FreeTextMinimumCharacters}, NoShuffle={self.NoShuffle}, PlayPercentageVideo={self.PlayPercentageVideo}, AllowNeitherBoth={self.AllowNeitherBoth}, SwapContextInstruction={self.SwapContextInstruction}, MuteVideo={self.MuteVideo})"
 
     def __repr__(self) -> str:
         return self.__str__()
