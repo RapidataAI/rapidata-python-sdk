9fcd640cf220c4f20f141a821d49a29fa7345ce4
Rudolf the Rabbit <login@rapidata.ai>
2026-07-28 14:05:24 +0000

    chore: update OpenAPI client to 2026.07.28.1353-4b5496d

--- src/rapidata/api_client/models/model_license_type.py
+++ src/rapidata/api_client/models/model_license_type.py
@@ -17,18 +17,19 @@ from enum import Enum
 from typing_extensions import Self
 
 
-class FlowType(str, Enum):
+class ModelLicenseType(str, Enum):
     """
-    FlowType
+    ModelLicenseType
     """
 
     """
     allowed enum values
     """
-    RANKING = 'Ranking'
+    OPENSOURCE = 'OpenSource'
+    PROPRIETARY = 'Proprietary'
 
     @classmethod
     def from_json(cls, json_str: str) -> Self:
-        """Create an instance of FlowType from a JSON string"""
+        """Create an instance of ModelLicenseType from a JSON string"""
         return cls(json.loads(json_str))
 
